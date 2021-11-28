437464c0d31b1e4f7d3c4dbc3e8a19dc34a927dc
A-PS1999 <sarnoldparra@gmail.com>
2021-11-28 18:09:39 +0000

    Styled sign up page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { registerUser, userSelector } from '../../store/userSlice.js';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import './SignUp.scss'
 import Navbar from '../Navbar/Navbar.js';
 import Footer from '../Footer/Footer.js';
 
@@ -31,16 +32,17 @@ export default function SignUp() {
 			<Navbar />
 			<div>
 				<main>
-					<div>
-						<form onSubmit={handleSubmit(submitData)}>
-							<div>
-								<input {...register("username", { required: true })} placeholder="Username" />
-								<input {...register("email", { required: true })} placeholder="E-Mail" />
-								<input {...register("password", { required: true })} placeholder="Password" />
-								<input {...register("confirmPassword", { required: true })} placeholder="Confirm Password" />
-								<button type="submit">Submit</button>
-							</div>
-						</form>
+					<h2 className="signup-heading">Sign Up</h2>
+					<div className="form-container">
+						<div className="signup-form">
+							<form onSubmit={handleSubmit(submitData)}>
+									<input {...register("username", { required: true })} placeholder="Username" />
+									<input {...register("email", { required: true })} placeholder="E-Mail" />
+									<input {...register("password", { required: true })} placeholder="Password" />
+									<input {...register("confirmPassword", { required: true })} placeholder="Confirm Password" />
+									<button type="submit">Submit</button>
+							</form>
+						</div>
 					</div>
 				</main>
 			</div>
