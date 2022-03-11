e1939675d5495bc81c89d2b6f1c5e4014306d115
Ben Koop <ben.koop@routable.com>
2022-03-11 07:42:51 -0500

    clean up fixed

--- src/components/Game/Card/Card.js
+++ src/components/Game/Card/Card.js
@@ -17,7 +17,10 @@ export default function Card({id, game_id, value, src, defaultVisibility, func})
     }, [value])
 
     useEffect(() => {
-        socket.on(`game:${game_id}:pickdealer-card-selected`, (data) => {
+        // set name here, helps avoid accidental typos
+        const eventName = `game:${game_id}:pickdealer-card-selected`;
+        //gets a reference to the handler, used so socket knows which handler to remove in socket.off
+        const handler = (data) => {
             if (data.cardId === id) {
                 setIsHidden(false);
                 setIsDisabled(true);
@@ -26,9 +29,10 @@ export default function Card({id, game_id, value, src, defaultVisibility, func})
             if (playerId && data.userId === playerId) {
                 dispatch(setHasClicked())
             }
-        })
+        }
+        socket.on(eventName, handler)
         return () => {
-            socket.off(`game:${game_id}:pickdealer-card-selected`);
+            socket.off(eventName, handler);
         }
     }, [dispatch, game_id, playerId, id])
 
